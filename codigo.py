34f6a0374435bf12dfef6bef93bf351738daa7c4
Julian Barcia <julianbarcia0@gmail.com>
2024-09-18 17:11:56 -0300

    subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,10 @@
+# Pedimos al usuario que ingrese los dos números
+numero1 = 5
+numero2 = 7
+# Realizamos las operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+
+# Imprimimos los resultados
+print("La suma de los números es:", suma)
+print("La resta de los números es:", resta)
