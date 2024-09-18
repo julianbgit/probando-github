f14462dff40e95f8eae795abad4d2ac24590d0ca
julian <julianbarcia@gmail.com>
2024-09-18 18:35:01 -0300

    cambiando el commit

--- codigo.py
+++ codigo.py
@@ -4,7 +4,8 @@ numero2 = 7
 # Realizamos las operaciones
 suma = numero1 + numero2
 resta = numero1 - numero2
-
+producto= numero1 * numero2
 # Imprimimos los resultados
 print("La suma de los números es:", suma)
 print("La resta de los números es:", resta)
+print("el producto de tus numeros:", producto)
